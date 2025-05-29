96da8f4b58782f09ff23a92381271322ad6f74ee
FacundoJarma <49127963@est.ort.edu.ar>
2025-05-29 11:54:22 -0300

    agregar la esfera blanca

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+
+    bool collisionWithPlayer(Collision c)
+    {
+       return c.gameObject.name == "Player";
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collisionWithPlayer(collision))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
