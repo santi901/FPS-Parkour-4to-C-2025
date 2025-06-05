194e674a706a43c882e8f0e6d7f59183271d7f9e
FacundoJarma <49127963@est.ort.edu.ar>
2025-06-05 12:05:07 -0300

    Mercancia y Dinero

--- Assets/Scripts/MercanciaScript.cs
+++ Assets/Scripts/MercanciaScript.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class MercanciaScript : MonoBehaviour
 {
+    public float cost;
+    public MoneyManager moneyManager;
+
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
 
     bool collisionWithPlayer(Collision c)
     {
-       return c.gameObject.name == "Player";
+        return c.gameObject.name == "Player";
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collisionWithPlayer(collision))
         {
+            moneyManager.UpdateMoney(cost);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+
+    public UIManager uiManager;
+    public float money;
+
+    void Start()
+    {
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+
+    public void UpdateMoney(float moneyToAdd)
+    {
+        if (money + moneyToAdd < 0)
+        {
+            Debug.Log("No te alcanza!");
+            return;
+        }
+        money += moneyToAdd;
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+}
